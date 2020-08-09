90388e08f47b3ebafb6c7aef83a94524dd35fe4e
hangfeng <nicehangfeng@foxmail.com>
2020-08-09 22:37:52 +0800

    Update index.js

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Fragment } from 'react';
 import ReactDOM from "react-dom";
-let arrdata = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
 
-ReactDOM.render(<WelcomeDialog />, document.getElementById("root"));
+function ListItem({item}) {
+  return (
+    <>
+      <dt>{item.term}</dt>
+      <dd>{item.desc}</dd>
+    </>
+  )
+}
+class Example extends React.Component {
+  render() {
+    return <ListItem item={{term: 1, desc: 'desc'}} />
+  }
+}
+
+ReactDOM.render(
+  <Example />,
+  document.getElementById('root')
+);
+
