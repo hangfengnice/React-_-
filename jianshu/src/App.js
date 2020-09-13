4020497f4b9af35d6256dbe6c64bdd06575a6ca9
hangfeng <nicehangfeng@foxmail.com>
2020-09-13 22:28:18 +0800

    feat

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -3,10 +3,13 @@ import {Provider} from 'react-redux'
 import Header from './common/header'
 import store from './store'
 
+import {Title} from './style'
+
 function App() {
   return (
     <Provider store={store}>
       <Header />
+      <Title >hello title</Title>
     </Provider>
   );
 }
