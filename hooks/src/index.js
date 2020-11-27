fbd88fa2abad0ed264165a1157bccbb7ec4b7678
hangfeng <nicehangfeng@foxmail.com>
2020-11-27 19:39:27 +0800

    react

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -2,17 +2,43 @@ import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 
-function App() {
-  const [name, setName] = useState('hangfeng')
-  const [yy, setYy] = useState('hangfeng')
+// function App() {
+//   const [name, setName] = useState('hangfeng')
+//   const [yy, setYy] = useState('hangfeng')
 
-  return <div>
-    <p>name: {name}</p>
-    <p>yy: {yy}</p>
-    <button onClick={() => setName(name + '1')}>click</button>
-    <button onClick={() => setYy(yy + '1')}>click</button>
-    butt
-  </div>;
+//   return <div>
+//     <p>name: {name}</p>
+//     <p>yy: {yy}</p>
+//     <button onClick={() => setName(name + '1')}>click</button>
+//     <button onClick={() => setYy(yy + '1')}>click</button>
+//     butt
+//   </div>;
+// }
+
+
+const valueKey = Symbol()
+
+function useFormInput() {
+  const [value, setValue] = useState(valueKey)
+
+  return {
+    value,
+    onChange(e) {
+      setValue(e.target.value)
+    }
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Form() {
+  const name = useFormInput()
+
+  return (
+    <div>
+      <input type="text" {...name} />
+    </div>
+  )
+}
+
+ReactDOM.render(<Form />, document.getElementById("root"));
+
+
